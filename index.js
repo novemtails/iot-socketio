31399d61bc9317564540a3c1a88a74b120bf71cd
Indiees <indiees.co@gmail.com>
2019-08-24 01:38:22 -0500

    Connection beetwen frontend and olet screen. Send messages from html and view in oled screen

--- index.js
+++ index.js
@@ -8,13 +8,23 @@ app.get('/', function(req, res){
 
 
 io.on('connection', function(socket){  
-  
-    socket.on('Connection', function(msg){
-        console.log('message #'  + ": " + msg);
-        socket.emit('event', "Hola NodeMCU, soy el servidor");
-      });
+  console.log('a user connected');
 
-     //socket.emit('ledOff', socket.id);
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('ledOn', msg);
+    console.log('On');
+  });
+  
+  socket.on('Connection', function(msg){
+    console.log('message #'  + ": " + msg);
+    socket.emit('event', "Hola NodeMCU, soy el servidor");
+    });
+    //socket.emit('ledOn', 'dsdsdsds');
 });
 
 http.listen(process.env.PORT || 3000, function(){
--- nodeMCU/socket/socket.ino
+++ nodeMCU/socket/socket.ino
@@ -1,24 +1,37 @@
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <SocketIoClient.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
 #define USE_SERIAL Serial
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+#define OLED_ADDR   0x3C
 
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
 ESP8266WiFiMulti WiFiMulti;
 SocketIoClient webSocket;
 
 void event(const char * payload, size_t length) {
-  USE_SERIAL.printf("got message: %s\n", payload);
+  USE_SERIAL.printf("got message: %s\n ZZZZZZ", payload);
 }
 
 int led = 2;
 bool ledOnStatus;
 
 void setup() {
-
+  // Display
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Welcome");
+  display.display();
+  
   pinMode(led, OUTPUT);
     USE_SERIAL.begin(115200);
 
@@ -45,6 +58,7 @@ void setup() {
     webSocket.emit("Connection", "\"Hola, soy nodeMCU\"");
     
     webSocket.on("connect", response);
+    webSocket.on("ledOn", ledOn);
     
     
     // use HTTP Basic Authorization this is optional remove if not needed
@@ -53,23 +67,33 @@ void setup() {
 
 void response(const char * response, size_t length) {
           USE_SERIAL.println(response);
+          display.println(response);
         }
 
 void ledOn(const char * ledOn, size_t length){
   USE_SERIAL.println("Led Encendido");
+  USE_SERIAL.println(ledOn);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);  
+  display.println(ledOn);
+  display.display();
   ledOnStatus = true;
   }
   void ledOff(const char * ledOff, size_t length){
     USE_SERIAL.println("Led Apagado");
   ledOnStatus = false;
   }
+
 void loop() {
+  display.clearDisplay();
     webSocket.loop();
     webSocket.on("ledOn", ledOn);
-    webSocket.on("ledOff", ledOff);
-
     if(ledOnStatus){
       digitalWrite(led, HIGH);
+      display.println("Led Encendido");
       }
     else{
       digitalWrite(led, LOW);
